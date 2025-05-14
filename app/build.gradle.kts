1a71b5e031fec7ada9f2ea0124d0e5efa26568eb
cuadratico <tirsi333@gmail.com>
2025-05-14 20:03:04 +0200

    New gradle.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.cyberoutine"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.0.4"
+        versionCode = 7
+        versionName = "0.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
